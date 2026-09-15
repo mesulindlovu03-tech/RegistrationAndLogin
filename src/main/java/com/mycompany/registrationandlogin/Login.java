59fb1590a47f7d3469ec1319971e4eef35644e26
Student <Student@LabVM2049939.mqrmsefk4x2ebpap24olwruutd.jnbx.internal.cloudapp.net>
2026-09-15 20:46:43 +0200

    initial project

---
+++ src/main/java/com/mycompany/registrationandlogin/Login.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.registrationandlogin;
+
+/**
+ *
+ * @author Student
+ */
+public class Login {
+    private String StoredUsername;
+    private String StoredPassword;
+    private String StoredCellPhone;
+    private String StoredFirstname;
+    private String StroredLastName;
+    private boolean isRegistered = false;
+    
+    public boolean CheckUserName(String username){
+        if (username ==null){
+            return false;
+        }
+        return username.contains("_") && username.length() <=5;
+    }
+}
