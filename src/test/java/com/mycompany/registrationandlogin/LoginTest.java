3b1ab07ac4a25404597f1f39b632465e754693f4
Student <Student@LabVM2049939.mqrmsefk4x2ebpap24olwruutd.jnbx.internal.cloudapp.net>
2026-09-17 09:54:13 +0200

    Added 6 JUnit tests for login class methods and fixed them

--- src/test/java/com/mycompany/registrationandlogin/LoginTest.java
+++ src/test/java/com/mycompany/registrationandlogin/LoginTest.java
@@ -43,7 +43,7 @@ public class LoginTest {
     @Test
     public void testCheckCellPhone_incorrectlyFormatted() {
         Login login = new Login();
-        assertTrue (login.CheckCellPhone("0123456789"));
+        assertFalse (login.CheckCellPhone("0123456789"));
     }
     @Test
     public void testLogin_successful() {
@@ -55,6 +55,43 @@ public class LoginTest {
     public void testLogin_Failed() {
         Login login = new Login();
         login.RegisterUser("mes_1", "Ch&&sec@ke99!", "+27123456789", "Mesuli", "Ndlovu");
-        assertTrue (login.LoginUser("mes_1", "WronPassword1!"));
+        assertFalse (login.LoginUser("mes_1", "WronPassword1!"));
+    }
+    @Test 
+    public void testRegisterUser_invalidUsername() {
+        Login login = new Login();
+        String result = login.RegisterUser("mesuli!!!!!!", "Ch&&secc@ke99!", "+27123456789", "Mesuli", "Ndlovu");
+        assertEquals("Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five character in length.", result);
+    }
+    @Test 
+    public void testRegisterUser_invalidPassword() {
+        Login login = new Login();
+        String result = login.RegisterUser("mes_1", "password", "+27123456789", "Mesuli", "Ndlovu");
+        assertEquals("Password is not correctly formatted; please ensure that your password contains at least eight characters,capital letter, a number, and a special character.", result);
+    }
+    @Test 
+    public void testRegisterUser_invalidCellPhone() {
+        Login login = new Login();
+        String result = login.RegisterUser("mes_1", "Ch&&secc@ke99!", "0123456789", "Mesuli", "Ndlovu");
+        assertEquals("Cellphone number is not correctly formatted or does not cotain an international code; please correct the number and try again.", result);
+    }
+    @Test 
+    public void testRegisterUser_success() {
+        Login login = new Login();
+        String result = login.RegisterUser("mes_1", "Ch&&secc@ke99!", "+27123456789", "Mesuli", "Ndlovu");
+        assertEquals("Registration successful", result);
+    }
+    @Test 
+    public void testReturnLoginStatus_success() {
+        Login login = new Login();
+        login.RegisterUser("mes_1", "Ch&&secc@ke99!","+27123456789", "Mesuli", "Ndlovu");
+        String result = login.ReturnLoginStatus("mes_1", "Ch&&secc@ke99!");
+        assertEquals("Welcome Mesuli Ndlovu it is good to see you", result);
+    }
+    @Test 
+    public void testReturnLoginStatus_Failed() {
+        Login login = new Login();
+        String result = login.ReturnLoginStatus("mesuli!!!!!!", "password");
+        assertEquals("Username or password incorrect, please try again", result);
     }
 }
